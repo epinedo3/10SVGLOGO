cca89b5458303318a4c5838b6a0d5dcf5d831f29
Enrique Pinedo <epinedo31@outlook.com>
2023-09-23 14:09:42 -0500

    finished all 3 shapes

--- index.js
+++ index.js
@@ -7,26 +7,36 @@ const inquirer = require("inquirer")
 inquirer.prompt([{
     type: 'input',
     name: 'text',
-    message: 'What text would you need in your shape? '
+    message: 'What 3 Characters would you like in the shape? '
 },{
     type: 'input',
     name: 'textColor',
-    message: 'What text color would you need in your shape? '
+    message: 'What color text would you like in the shape? '
 },{
     type: 'list',
     name: 'shape',
-    message: 'What shape would you need? ',
+    message: 'What shape would you like? ',
     choices: ['circle', 'triangle', 'square']
 
 },{
     type: 'input',
     name: 'shapeColor',
-    message: 'What shape color would you need?'
+    message: 'What color shape would you like?'
 }])
 
 .then(response => {
     if(response.shape === "circle"){
         const circle = new Circle (response.text, response.textColor, response.shape, response.shapeColor)
         fs.writeFileSync("./dist/logo.svg",circle.render())
+    }
+
+    if(response.shape === "triangle"){
+        const triangle = new Triangle (response.text, response.textColor, response.shape, response.shapeColor)
+        fs.writeFileSync("./dist/logo.svg",triangle.render())
+    }
+
+    if(response.shape === "square"){
+        const square = new Square (response.text, response.textColor, response.shape, response.shapeColor)
+        fs.writeFileSync("./dist/logo.svg",square.render())
     }
 })
